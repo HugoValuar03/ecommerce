16c0ef83ba4f5cf7a50aa519d06804b7d335b350
Hugo Valuar Bailona <hugovaluarb03@gmail.com>
2024-04-25 21:53:29 -0300

    Testes marca
    Realizando os testes da marca

--- src/test/java/br/unitins/topicos1/resource/MarcaResourceTest.java
+++ src/test/java/br/unitins/topicos1/resource/MarcaResourceTest.java
@@ -1,15 +1,18 @@
 package br.unitins.topicos1.resource;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.everyItem;
+
 import org.junit.jupiter.api.Test;
 
 import br.unitins.topicos1.dto.MarcaDTO;
+import br.unitins.topicos1.service.MarcaService;
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MediaType;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.Matchers.everyItem;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class MarcaResourceTest {
@@ -17,6 +20,9 @@ public class MarcaResourceTest {
     //hasItem(): É usado para verificar se uma determinada coleção contém um item específico.
     //is(): é usado para verificar se o resultado de um teste é igual a um valor esperado. Ele pode ser usado para comparar valores simples ou objetos complexos.
     //everyItem(): é usado para verificar se todos os elementos de uma coleção atendem a determinados critérios. Ele é útil para verificar se todos os elementos de uma coleção satisfazem uma determinada condição.
+
+    @Inject
+    public MarcaService marcaService;
 
     @Test
     public void createTest(){
@@ -26,7 +32,7 @@ public class MarcaResourceTest {
             .contentType(MediaType.APPLICATION_JSON)
             .body(dto)
         .when()
-            .post("/admin/marca")
+            .post("/marcas")
         .then()
             .statusCode(201)
             .body("nome", is("Canon"));
@@ -36,30 +42,56 @@ public class MarcaResourceTest {
     public void findAllTest(){
         given()
             .when()
-            .get("/admin/marca")
+            .get("/marcas")
             .then()
-            .statusCode(200);
+            .statusCode(200)
+            .body("nome", hasItem("Canon"));
     }
 
     @Test
     public void findByIdTest(){
         given()
             .when()
-            .get("/admin/marca/2")
+            .get("/marcas/1")
             .then()
             .statusCode(200)
-            .body("id", is(2));
-
+            .body("idMarca", equalTo(1));
     }
 
     @Test
     public void findByNomeTest(){
         given()
             .when()
-            .get("/admin/marca/search/Canon")
+            .get("/marcas/search/nome/Canon")
             .then()
             .statusCode(200)
-            .body("nome", everyItem(is("Canon")));
+            .body("nome", everyItem(is("Canon")));  
+    }
+
+    @Test
+    public void updateTest() {
+        
+        MarcaDTO dto = new MarcaDTO("Fujifilm");
+
+        given()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(dto)
+        .when()
+            .pathParam("id", 3)
+            .put("/marcas/{id}")
+        .then()
+            .statusCode(204);
+  
+    }
+
+    @Test
+    public void deleteTest() {
+        given()
+            .when()
+            .pathParam("id", 3)
+            .delete("/marcas/{id}")
+            .then()
+            .statusCode(204);
     }
 
 }
